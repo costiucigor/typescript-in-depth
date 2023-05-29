b78c1475b60d0164c684eeae405683548d911c3a
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-05-29 23:45:14 +0300

    fixed task

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import {ref, computed, onMounted} from "vue";
 import { useRoute } from "vue-router";
 import store from "../store";
+import { de } from "vuetify/locale";
+import router from "../router";
+
+const rules = [value => value.length <= 200 || 'Max 200 characters'];
+const description = ref("");
+const date = ref("");
+const tags = ref<{ [key: number]: { id: number; description: string } }>({});
+const newTag = ref('');
+
+const addTag = () => {
+  const trimmedTag = newTag.value.trim();
+  if (trimmedTag) {
+    const nextKey = Object.keys(tags.value).length + 1;
+    tags.value[nextKey] = { id: nextKey, description: trimmedTag };
+    newTag.value = "";
+  }
+};
+
+const tagEntries = computed(() => Object.entries(tags.value));
+
+const submitHandler = () => {
+}
+
+onMounted(() => {
+  if (task.value) {
+    tags.value = task.value.tags;
+    description.value = task.value.description;
+    date.value = task.value.date;
+  }
+})
 
 const route = useRoute();
 const taskId = Number(route.params.id);
@@ -11,8 +41,55 @@ const task = computed(() => store.getters.taskById(taskId));
 <template>
   <div>
     <div v-if="task">
-      <h1>{{ task.title }}</h1>
-      <p>{{ task.description }}</p>
+
+      <v-form @submit.prevent="submitHandler">
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <p>{{ task.title }}</p>
+              <p>{{ task.description }}</p>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field v-model="title" label="Title" required></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-chip v-for="(tag, key) in tags" :key="tag" label outlined class="ma-2" closable required>
+                {{ tag.description }}
+              </v-chip>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-textarea
+                  :rules="rules"
+                  label="Write a description"
+                  no-resize
+                  rows="10"
+                  v-model="description"
+                  counter
+                  required
+              ></v-textarea>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field
+                  type="date"
+                  v-model="date"
+                  class="mt-4"
+                  required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-btn class="btn" type="submit">
+            Create Task
+          </v-btn>
+        </v-container>
+      </v-form>
     </div>
     <p v-else>404 Task not found</p>
   </div>
